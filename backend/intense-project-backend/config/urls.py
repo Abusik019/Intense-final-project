14f0d6395bc6c8a76e4b5bb104c69517401c8be3
zabit <xaclafun1991@gmail.com>
2024-05-31 01:49:13 +0300

    .

--- backend/intense-project-backend/config/urls.py
+++ backend/intense-project-backend/config/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),     # Маршрут для пользовательских представлений.
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),    # Маршрут для получения JWT токена.
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),   # Маршрут для обновления JWT токена.
 ]
--- backend/intense-project-backend/users/models.py
+++ backend/intense-project-backend/users/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Модель пользователя.
+    """
+    pass
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f'{self.username}'
+
+
+class Category(models.Model):
+    """
+    Модель категорий.
+    """
+    title = models.CharField('Категория', max_length=128)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Likes(models.Model):
+    """
+    Модель лайков.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='liked_posts')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+
+
+class Favorites(models.Model):
+    """
+    Модель избранных.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorite_posts')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='favorites')
