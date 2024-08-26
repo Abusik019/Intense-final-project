aa607746487c743770b29e8d93a70a8ec60af6dc
Abusik019 <abusroyale@gmail.com>
2024-08-26 14:01:37 +0300

    fix bugs

--- frontend/intense-app/src/pages/Search/index.jsx
+++ frontend/intense-app/src/pages/Search/index.jsx
@@ -5,6 +5,8 @@ import { TagsSearch } from "../../components/TagsSearch";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getArticles } from "../../store/slices/articles";
+import favoriteLogo from '../../assets/is-favorite.svg';
+import notFavoriteLogo from '../../assets/not-favorite.svg'
 
 function Search() {
     const dispatch = useDispatch();
@@ -27,7 +29,15 @@ function Search() {
     
 
     const handleInputChange = (e) => {
-        setInputValue(e.target.value);
+        const value = e.target.value;
+        setInputValue(value);
+
+        const filteredArticles = articles.filter((article) => 
+            article.title.includes(value)
+        );
+
+        setArticlesResult(filteredArticles);
+        console.log("articlesResult:", filteredArticles);
     };
 
     const handleTagClick = (tagName) => {
@@ -42,6 +52,26 @@ function Search() {
     return (
         <div className={styles.search}>
             <SearchInput inputValue={inputValue} handleInputChange={handleInputChange}/>
+            {inputValue && articlesResult.length > 0 && 
+                <ul className={styles.searchResults}>
+                    {articlesResult.map(article => (
+                        <li key={article.id}>
+                            <div className={styles.topSide}>
+                                <h1>{article.author}</h1>
+                                <h2>{article.category.title}</h2>
+                                <h3>{article.time_to_read} min</h3>
+                            </div>
+                            <div className={styles.bottomSide}>
+                                <div className={styles.bottomLeftSide}>
+                                    <img src={article.image} alt="article"/>
+                                    <a href={`/articles/${article.id}`}>{article.title}</a>
+                                </div>
+                                <img src={article.is_favorite ? favoriteLogo : notFavoriteLogo} alt="favorite status"/>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            }
             <TagsSearch handleTagClick={handleTagClick}/>
         </div>
     );
